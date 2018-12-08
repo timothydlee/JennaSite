26ebf40d2d52fdbc8cb9d696c138a1eb3d109267
Tim Lee <timothy.lee@intlfcstone.com>
2018-12-08 14:32:40 -0500

    Removing and starting again.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Jenna's Site
-Jenna Green's personal website, including links to her portfolio and videos of her dance performances.
--- server/main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-var app = new express();
-
-app.get('/', function(req, res) {
-    res.render('./../app/index.ejs', {});
-})
-.listen(7777);
